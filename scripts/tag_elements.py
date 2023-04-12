7381b61e28167b0b5376bbebf5cf62ec459a84fa
yagdias <yag.dias@gmail.com>
2023-04-12 18:19:19 -0300

    Resolving problemns encounter in review

--- scripts/tag_elements.py
+++ scripts/tag_elements.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-def lista_para_string(lst):
-    return ','.join(map(str, lst))
+def _list_to_string(overlaped_elements: list):
+    return ','.join(map(str, overlaped_elements))
 
 def tag_elements(tax_file, log):
 
     df = pd.read_csv(tax_file, sep='\t')
 
-    # Tratamento para adicionar colunas para aplicação da tag
+    # Adding columns for tagging elements
     df['Element-ID'] = df['Element-ID'].replace('.*/','', regex=True)
     df['start'] = df['Element-ID'].replace('-.*','', regex=True)
     df['start'] = df['start'].replace('.*:', '', regex=True)
@@ -16,29 +16,30 @@ def tag_elements(tax_file, log):
     df['start'] = df['start'].astype(int)
     df['end'] = df['end'].astype(int)
 
-    # Indentificando linhas que existe overlap
+    # Identify elements that overlap
     matched_elements = []
     for i, row in df.iterrows():
         matched_ids = []
         filtered_df = df[df['contig'] == row['contig']]
         for j, other_row in filtered_df.iterrows():
-            if i != j and other_row['start'] <= row['end'] + 50 and other_row['end'] >= row['start'] - 50 and other_row['Family'] != row['Family']:
+            if i != j and other_row['start'] <= row['end'] + 100 and other_row['end'] >= row['start'] - 100 and other_row['Family'] != row['Family']:
                 matched_ids.append(other_row['Element-ID'])
         matched_elements.append(matched_ids)
 
-    # Atribuir a lista matched_elements à coluna 'Overlaped_Element_ID'
+    # Adding matched_elements to column 'Overlaped_Element_ID'
     df['Overlaped_Element_ID'] = matched_elements
 
-    # Tratamento das colunas
-    df['Overlaped_Element_ID'] = df['Overlaped_Element_ID'].apply(lista_para_string)
+    # Treating tags
+    df['Overlaped_Element_ID'] = df['Overlaped_Element_ID'].apply(_list_to_string)
     df.drop(columns=['start', 'end', 'contig'], inplace=True)
 
 
-    # Adicionando tag
+    # Adding tag
     for i, row in df.iterrows():
         if row['Overlaped_Element_ID'] == '':
             df.at[i, 'tag'] = 'unique'
-        if row['Overlaped_Element_ID'] != '':
+        else:
+            row['Overlaped_Element_ID'] != ''
             df.at[i, 'tag'] = 'overlaped'
 
     df.to_csv(tax_file, sep="\t", index=False, header=True)
