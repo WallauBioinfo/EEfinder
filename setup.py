463013babe90784c0cd05ed8a73a10cdc87aa1e1
dezordi <filipe.dezordi@varsomics.com>
2023-07-10 13:59:26 -0300

    change logic of create outdir

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     Tool to analyze endogenous elements (EEs) based on similarity search
     and junctions of genomic regions.
     """,
-    version="0.2.0",
+    version="0.3.0",
     authors="Filipe Dezordi and Yago Dias",
     authors_emails='zimmer.filipe@gmail.com" and yag.dias@gmail',
     classifiers=[
