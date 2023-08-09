09c209b25efa8dbd42806bf8626a819d3426b16b
yagdias <yag.dias@gmail.com>
2023-08-09 00:54:06 -0300

    first part of the formating and addintion

--- eefinder/clean_data.py
+++ eefinder/clean_data.py
@@ -7,16 +7,16 @@ class RemoveShortSequences:
 
     Keyword arguments:
     input_file: input fasta file
-    cutoff: cutoff length
+    cutoff: cutoff length, parsed by -ln
     """
 
-    def __init__(self, input_file, cutoff):
+    def __init__(self, input_file: str, cutoff: str) -> object:
         self.input_file = input_file
         self.cutoff = cutoff
 
         self.cut_seq()
 
-    def cut_seq(self):
+    def cut_seq(self) -> None:
         new_sequences = []
         input_handle = open(self.input_file, "r")
         output_handle = open(self.input_file + ".fmt", "w")
@@ -31,17 +31,17 @@ class MaskClean:
     Remove sequences of EE on regions with a certain % of soft masked bases.
 
     Keyword arguments:
-    input_file - fasta file, with putative EVEs.
-    m_per - treshold masked percentage value, parsed with -mp argument.
+    input_file: fasta file, with putative EEs
+    m_per: treshold masked percentage value, parsed with -mp argument
     """
 
-    def __init__(self, input_file, m_per):
+    def __init__(self, input_file: str, m_per:str) -> object:
         self.input_file = input_file
         self.m_per = m_per
 
         self.mask_clean()
 
-    def mask_clean(self):
+    def mask_clean(self) -> None:
         sequences = {}
         for seq_record in SeqIO.parse(self.input_file, "fasta"):
             sequence = str(seq_record.seq)
